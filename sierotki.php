980516dcf9df6536636c6ebd2b9eb39861a4cd7a
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-18 08:23:15 +0000

    * NEW: add filter comment_text. * BUGFIX: capital letters was missing by plugin.
    git-svn-id: https://plugins.svn.wordpress.org/sierotki/trunk@347255 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sierotki.php
+++ sierotki.php
@@ -11,15 +11,16 @@ Author URI: http://iworks.pl/
 function iworks_orphan($content)
 {
     if ( $content ) {
-        return preg_replace('/([ >]+)([aiouwz]|że|za|na|od|nad|pod|to) +/', "$1$2&nbsp;", $content);
+        return preg_replace('/([ >]+)([aiouwz]|że|za|na|od|nad|pod|to) +/i', "$1$2&nbsp;", $content);
     }
     return $content;
 }
 
 function iworks_orphan_init()
 {
-    add_filter('the_excerpt', 'iworks_orphan');
-    add_filter('the_content', 'iworks_orphan');
+    add_filter('comment_text', 'iworks_orphan');
+    add_filter('the_content',  'iworks_orphan');
+    add_filter('the_excerpt',  'iworks_orphan');
 }
 
 add_action('init', 'iworks_orphan_init');
