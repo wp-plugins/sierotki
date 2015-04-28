029202514816fa10f7610d8b693439ca30084e2e
iworks <iworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 21:06:55 +0000

    add iworks_orphan_replace filter
    git-svn-id: https://plugins.svn.wordpress.org/sierotki/trunk@1148430 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/iworks/orphan.php
+++ vendor/iworks/orphan.php
@@ -18,64 +18,57 @@ class iworks_orphan
         /**
          * l10n
          */
-        load_plugin_textdomain( 'iworks_orphan', false, dirname( plugin_basename( dirname(dirname(__FILE__))) ).'/languages' );
+        load_plugin_textdomain('iworks_orphan', false, dirname( plugin_basename( dirname(dirname(__FILE__))) ).'/languages' );
 
         /**
          * actions
          */
-        add_action( 'init',       array( &$this, 'init' ) );
-        add_action( 'admin_init', array( &$this, 'admin_init' ) );
-        add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+        add_action('init',       array( &$this, 'init' ) );
+        add_action('admin_init', array( &$this, 'admin_init' ) );
+        add_action('admin_menu', array( &$this, 'admin_menu' ) );
 
         /**
          * options
          */
         $this->options = array (
             'comment_text' => array(
-                'description' => __( 'Use for comments:',
-                'iworks_orphan' ),
+                'description' => __('Use for comments:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the comments.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'the_title'    => array(
-                'description' => __( 'Use for post title:',
-                'iworks_orphan' ),
+                'description' => __('Use for post title:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the post_title.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'the_excerpt'  => array(
-                'description' => __( 'Use for excerpt:',
-                'iworks_orphan' ),
+                'description' => __('Use for excerpt:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the excerpt.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'the_content'  => array(
-                'description' => __( 'Use for content:',
-                'iworks_orphan' ),
+                'description' => __('Use for content:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the content.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'woocommerce_product_title'  => array(
-                'description' => __( 'Use for WooCommerce product title:',
-                'iworks_orphan' ),
+                'description' => __('Use for WooCommerce product title:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the WooCommerce product title.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'woocommerce_short_description'  => array(
-                'description' => __( 'Use for WooCommerce short description:',
-                'iworks_orphan' ),
+                'description' => __('Use for WooCommerce short description:', 'iworks_orphan' ),
                 'type'  => 'checkbox',
                 'label' => __('Enabled the substitution of orphans in the WooCommerce short description.', 'iworks_orphan'),
                 'sanitize_callback' => 'absint',
             ),
             'own_orphans'  => array(
-                'description' => __( 'User definied orphans:',
-                'iworks_orphan' ),
+                'description' => __('User definied orphans:', 'iworks_orphan' ),
                 'type' => 'text',
                 'label' => __('Use a comma to separate orphans.', 'iworks_orphan'),
                 'sanitize_callback' => 'esc_html',
@@ -110,17 +103,21 @@ class iworks_orphan
             'za', 'ze',
             'że', 'żeby', 'żebyś',
         );
-        $own_orphans = trim( get_option( 'iworks_orphan_own_orphans', '' ), ' \t,');
+        $own_orphans = trim( get_option('iworks_orphan_own_orphans', '' ), ' \t,');
         if ( $own_orphans ) {
             $own_orphans = preg_replace('/\,\+/', ',', $own_orphans);
             $therms = array_merge( $therms, preg_split('/,[ \t]*/', strtolower( $own_orphans ) ) );
         }
+        $therms = apply_filters('iworks_orphan_therms', $therms);
+        /**
+         * base therms replace
+         */
         $re = '/^([aiouwz]|'.preg_replace('/\./', '\.', implode('|', $therms)).') +/i';
         $content = preg_replace( $re, "$1$2&nbsp;", $content );
         /**
          * replace space in numbers
          */
-        $content = preg_replace( '/(\d) (\d)/', "$1&nbsp;$2", $content );
+        $content = preg_replace('/(\d) (\d)/', "$1&nbsp;$2", $content );
         /**
          * single letters
          */
@@ -175,7 +172,7 @@ class iworks_orphan
                     '<label for="%s"><input type="checkbox" name="%s" value="1"%s id="%s"/> %s</label>',
                     $field,
                     $field,
-                    (get_option($field, 1) == 1)?' checked="checked"':'',
+                    checked(1, get_option($field, 1), false),
                     $field,
                     isset($option['label'])? $option['label']:'&nbsp;'
                 );
@@ -202,22 +199,22 @@ class iworks_orphan
         <div class="postbox-container" style="width:23%;margin-left:2%">
             <div class="metabox-holder">
                 <div id="links" class="postbox">
-                    <h3 class="hndle"><?php _e( 'Loved this Plugin?', 'iworks_orphan' ); ?></h3>
+                    <h3 class="hndle"><?php _e('Loved this Plugin?', 'iworks_orphan' ); ?></h3>
                     <div class="inside">
-                        <p><?php _e( 'Below are some links to help spread this plugin to other users', 'iworks_orphan' ); ?></p>
+                        <p><?php _e('Below are some links to help spread this plugin to other users', 'iworks_orphan' ); ?></p>
                         <ul>
-                            <li><a href="http://wordpress.org/extend/plugins/sierotki/"><?php _e( 'Give it a 5 star on Wordpress.org', 'iworks_orphan' ); ?></a></li>
-                            <li><a href="http://wordpress.org/extend/plugins/sierotki/"><?php _e( 'Link to it so others can easily find it', 'iworks_orphan' ); ?></a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/sierotki/"><?php _e('Give it a 5 star on Wordpress.org', 'iworks_orphan' ); ?></a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/sierotki/"><?php _e('Link to it so others can easily find it', 'iworks_orphan' ); ?></a></li>
                         </ul>
                     </div>
                 </div>
                 <div id="help" class="postbox">
-                    <h3 class="hndle"><?php _e( 'Need Assistance?', 'iworks_orphan' ); ?></h3>
+                    <h3 class="hndle"><?php _e('Need Assistance?', 'iworks_orphan' ); ?></h3>
                     <div class="inside">
-                        <p><?php _e( 'Problems? The links bellow can be very helpful to you', 'iworks_orphan' ); ?></p>
+                        <p><?php _e('Problems? The links bellow can be very helpful to you', 'iworks_orphan' ); ?></p>
                         <ul>
-                            <li><a href="<?php _e( 'http://wordpress.org/support/plugin/sierotki', 'iworks_orphan' ); ?>"><?php _e( 'Wordpress Help Forum', 'iworks_orphan' ); ?></a></li>
-                            <li><a href="mailto:<?php echo antispambot('marcin@iworks.pl'); ?>"><?php echo antispambot( 'marcin@iworks.pl' ); ?></a></li>
+                            <li><a href="<?php _e('http://wordpress.org/support/plugin/sierotki', 'iworks_orphan' ); ?>"><?php _e('Wordpress Help Forum', 'iworks_orphan' ); ?></a></li>
+                            <li><a href="mailto:<?php echo antispambot('marcin@iworks.pl'); ?>"><?php echo antispambot('marcin@iworks.pl' ); ?></a></li>
                         </ul>
                         <hr />
                         <p class="description"><?php _e('Created by: ', 'iworks_orphan' ); ?> <a href="http://iworks.pl/"><span>iWorks.pl</span></a></p>
@@ -230,9 +227,9 @@ class iworks_orphan
 
     public function admin_menu()
     {
-        if ( function_exists( 'add_theme_page' ) ) {
-            $this->admin_page = add_theme_page( __( 'Orphan', 'iworks_orphan'),  __('Orphan', 'iworks_orphan'), 'manage_options', basename(__FILE__), array( &$this, 'option_page' ) );
-            add_action( 'load-'.$this->admin_page, array( &$this, 'add_help_tab' ) );
+        if ( function_exists('add_theme_page' ) ) {
+            $this->admin_page = add_theme_page( __('Orphan', 'iworks_orphan'),  __('Orphan', 'iworks_orphan'), 'manage_options', basename(__FILE__), array( &$this, 'option_page' ) );
+            add_action('load-'.$this->admin_page, array( &$this, 'add_help_tab' ) );
         }
     }
 
@@ -245,17 +242,17 @@ class iworks_orphan
         // Add my_help_tab if current screen is My Admin Page
         $screen->add_help_tab( array(
             'id'    => 'overview',
-            'title' => __( 'Orphans', 'iworks_orphan' ),
-            'content'   => '<p>' . __( 'Plugin fix some Polish gramary rules with orphans.', 'iworks_orphan' ) . '</p>',
+            'title' => __('Orphans', 'iworks_orphan' ),
+            'content'   => '<p>' . __('Plugin fix some Polish gramary rules with orphans.', 'iworks_orphan' ) . '</p>',
         ) );
             /**
              * make sidebar help
              */
             $screen->set_help_sidebar(
-                '<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
-                '<p>' . __( '<a href="http://wordpress.org/extend/plugins/sierotki/" target="_blank">Plugin Homepage</a>', 'iworks_orphan' ) . '</p>' .
-                '<p>' . __( '<a href="http://wordpress.org/support/plugin/sierotki/" target="_blank">Support Forums</a>', 'iworks_orphan' ) . '</p>' .
-                '<p>' . __( '<a href="http://iworks.pl/en/" target="_blank">break the web</a>', 'iworks_orphan' ) . '</p>'
+                '<p><strong>' . __('For more information:' ) . '</strong></p>' .
+                '<p>' . __('<a href="http://wordpress.org/extend/plugins/sierotki/" target="_blank">Plugin Homepage</a>', 'iworks_orphan' ) . '</p>' .
+                '<p>' . __('<a href="http://wordpress.org/support/plugin/sierotki/" target="_blank">Support Forums</a>', 'iworks_orphan' ) . '</p>' .
+                '<p>' . __('<a href="http://iworks.pl/en/" target="_blank">break the web</a>', 'iworks_orphan' ) . '</p>'
             );
 
     }
@@ -266,7 +263,7 @@ class iworks_orphan
             $sanitize_callback = isset($option['sanitize_callback'])? $option['sanitize_callback']:null;
             register_setting('iworks_orphan', 'iworks_orphan_'.$filter, $sanitize_callback);
         }
-        add_filter( 'plugin_row_meta', array( &$this, 'links' ), 10, 2 );
+        add_filter('plugin_row_meta', array( &$this, 'links' ), 10, 2 );
     }
 
     public function init()
@@ -289,10 +286,11 @@ class iworks_orphan
             update_option('iworks_orphan_initialized', 1);
         }
         foreach ( array_keys( $this->options ) as $filter ) {
-            if ( 1 == get_option( 'iworks_orphan_'.$filter, 1 ) ) {
-                add_filter( $filter, array( &$this, 'replace' ) );
+            if ( 1 == get_option('iworks_orphan_'.$filter, 1 ) ) {
+                add_filter($filter, array($this, 'replace'));
             }
         }
+        add_filter('iworks_orphan_replace', array($this, 'replace'));
     }
 
     public function links($links, $file)
